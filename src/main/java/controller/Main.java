66e16735fc364241476edbbe53bc92b797c6d1f9
kozlowski-alexis <alexis.kozlowski43@gmail.com>
2020-01-21 19:58:17 +0100

    Debut de l'ajout de la recuperation des logs et affichage dans un select

--- src/main/java/controller/Main.java
+++ src/main/java/controller/Main.java
@@ -1,14 +1,34 @@
 package controller;
 
+import java.sql.Connection;
+import java.util.List;
+
+import model.DAOException;
+import model.DAOFactory;
+import model.DAOLog;
+import model.bean.Log;
 import view.Logs;
 import view.TchatIndex;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-//		TchatIndex tchatIndex = new TchatIndex();
-		Logs logs = new Logs();
+Connection c = null;
+		
+		try {
+			c = DAOFactory.getConnection();
+			DAOLog logDAO = DAOFactory.getDAOLog(c);
+			
+			final List<Log> logList = logDAO.list();
+			
+			for (Log log : logList) {
+			    System.out.println(log.getId()+" / "+log.getDate()+" / "+log.getUserName()+" / "+log.getMessage());
+			}
+			
+		} catch (DAOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
